a10901323a29f340fd99dd86218342390cf6f17d
dpryan79 <dpryan79@gmail.com>
2017-07-03 14:32:10 +0200

    It seems to work with parkour now, at least for MiSeq runs and presumably NextSeq too.

--- README.md
+++ README.md
@@ -62,10 +62,9 @@ The general workflow of this pipeline is as follows:
      * Everything about these PDFs is hard-coded. In an ideal world, this would have some sort of plugin interface.
   10. FastQC and fastq files are copied to the group directories, under `sequencing_data/`.
       * If the directories already exist then an error is produced. This is to ensure that nothing is inadvertently over-written!
-      * Only projects starting with the letters "A" or "C" will be distributed. Those starting with "A" are distributed to the groups and those with "C" to Andreas (`[Paths]->DEEPDir`).
-      * Projects starting with "B" are uploaded to the F\*EX server and an email with the link sent to "Uni"->"default" or "Uni"->"Schuele". The latter only occurs for DEEP data from the Scheule group.
-      * Projects starting with "A" are linked into Galaxy, if and only if the associated group has a data library with a "sequencing data" folder.
-      * Output directories and files for projects starting with "A" have their permissions changed to ensure that groups do not have write access.
+      * Projects with a matching local group (a subdirectory in `[Paths]`->`outputDir`) are transfered and linked into Galaxy
+      * Projects with no matching group are uploaded to the F\*EX server and an email with the link sent to "Uni"->"default".
+      * Projects with a matching local group have Output directories and files for projects starting with "A" have their permissions changed to ensure that groups do not have write access.
   11. A summary email is produced (largely by parsing `Stats/DemultiplexingStats.xml`) and sent to the email addresses specified via `[Email]`->`finishedTo`.
       * Note the other options under `[Email]`, which specify the host name of the outgoing email server and the outgoing email address.
   12. A file named `fastq.made` is produced in `[Paths]`->`outputDir`/`runID`/.
@@ -88,7 +87,6 @@ The configuration file is a human readable text file named `bcl2fastq.ini` and m
     * `outputDir` - The base directory where the demultiplexed fastq files and fastQC/md5sum output should be written.
     * `seqFacDir` - The base directory readable by the sequencing facility, for the files they're interested in.
     * `groupDir` - The base directory holding all group's datasets (currently, this should be `/data` for us).
-    * `DEEPDir` - The base directory holding all DEEP datasets.
     * `logDir` - The demultiplexing log for each run is written here.
   * `[FastQC]`
     * `fastqc_command` - Either just `fastqc` or possibly the full path, as appropriate.
--- bcl2fastq_pipeline/findFlowCells.py
+++ bcl2fastq_pipeline/findFlowCells.py
@@ -11,6 +11,8 @@ import smtplib
 import glob
 from email.mime.text import MIMEText
 import syslog
+import xml.etree.ElementTree as ET
+
 
 #Returns True on processed, False on unprocessed
 def flowCellProcessed(config) :
@@ -24,6 +26,168 @@ def flowCellProcessed(config) :
     return False
 
 
+# Get the number of lanes in the run. This might not match the number of lanes in the sampleSheet
+def getNumLanes(d):
+    try:
+        tree = ET.parse("{}/RunInfo.xml".format(d))
+        root = tree.getroot()[0]
+        numLanes = root.findall("FlowcellLayout")[0]
+        return int(numLanes.get("LaneCount"))
+    except:
+        return 1
+
+
+# For MiSeq and HiSeq 2500 runs, the i5 primer must be reverse complemented
+def mustRevComp(d):
+    FCID = d.strip("/").split("/")[-1]
+    # The type is designated by the first 1-2 letters of the machine name
+    t = FCID[7:]
+    if t.startswith("M"):
+        return True
+    elif t.startswith("SN"):
+        return True
+    return False
+
+
+def revComp(s):
+    """Reverse complement a primer sequence"""
+    d = {"A": "T", "C": "G", "G": "C", "T": "A", "N": "N"}
+    s = s[::-1]
+    x = [d[c] for c in s]
+    return "".join(x)
+
+
+def formatHeaderLine(cols, colLabs, indexCols, storeLanes):
+    """Format a single header line in a sample sheet"""
+    l = []
+    if storeLanes is True:
+        l.append("Lane")
+    if colLabs[1] is not None:
+        l.append("Sample_ID")
+    if colLabs[2] is not None:
+        l.append("Sample_Name")
+    if indexCols[0] is not None and len(cols[indexCols[0]]) > 0:
+        l.append("index")
+    if indexCols[1] is not None and len(cols[indexCols[1]]) > 0:
+        l.append("index2")
+    if colLabs[3] is not None:
+        l.append("Sample_Project")
+    return ",".join(l)
+
+
+def formatLine(cols, colLabs, indexCols, storeLanes, rcI5):
+    """Format a single line in a sample sheet"""
+    l = []
+    if storeLanes is True and colLabs[0] is not None:
+        l.append(cols[colLabs[0]])
+    if colLabs[1] is not None:
+        l.append(cols[colLabs[1]])
+    if colLabs[2] is not None:
+        l.append(cols[colLabs[2]])
+    if indexCols[0] is not None and len(cols[indexCols[0]]) > 0:
+        l.append(cols[indexCols[0]])
+    if indexCols[1] is not None and len(cols[indexCols[1]]) > 0:
+        if rcI5 is True:
+            l.append(revComp(cols[indexCols[1]]))
+        else:
+            l.append(cols[indexCols[1]])
+    if colLabs[3] is not None:
+        l.append(cols[colLabs[3]])
+    return ",".join(l)
+
+
+def reformatSS(rv):
+    """This is used in parseSampleSheet to reformat the output for upstream use"""
+    ss = []
+    laneOut = []
+    bcLens = []
+    nLanes = 0
+
+    for k, v in rv.items():
+        ss.append("\n".join(v[0]))
+        lanes = ""
+        if len(v[1]) > 0:
+            nLanes += len(v[1])
+            lanes = "_".join(["{}".format(x) for x in sorted(list(v[1]))])
+        laneOut.append(lanes)
+        bcLens.append(v[2])
+
+    if len(ss) < 2 and nLanes == 8:
+        laneOut = None
+    return ss, laneOut, bcLens
+
+        
+def parseSampleSheet(ss):
+    """
+    Return a dictionary with keys: (Barcode length 1, Barcode length 2)
+
+    return ss, laneOut, bcLens
+    """
+    rv = dict()
+
+    # If this is a NextSeq or a HiSeq 2500 rapid run, then don't store the incorrect Lane column
+    storeLanes = True
+    if getNumLanes(os.path.dirname(ss)) < 8:
+        storeLanes = False
+
+    # For NextSeq/MiSeq, reverse complement I5 sequences
+    rcI5 = mustRevComp(os.path.dirname(ss))
+    
+    f = open(ss)
+    inData = False
+    lastLine = None
+    colLabs = [None, None, None, None] # Lane, Sample_ID, Sample_Name, Sample_Project
+    indexCols = [None, None] # index, index2
+    for line in f:
+        bcLen = '0,0'
+        if inData is False:
+            if line.startswith("[Data]"):
+                inData = True
+                continue
+        else: 
+            cols = line.strip().split(",")
+            if lastLine is True:
+                if indexCols[0] is not None:
+                    bcLen = "{}".format(len(cols[indexCols[0]]))
+                    if indexCols[1] is not None:
+                        bcLen = "{},{}".format(bcLen, len(cols[indexCols[1]]))
+                    else:
+                        bcLen += ",0"
+
+                # Append to rv, with header
+                if bcLen not in rv:
+                    rv[bcLen] = [["[Data]", formatHeaderLine(cols, colLabs, indexCols, storeLanes)], set(), bcLen]
+
+                # Add the lane to the set, if relevant
+                if colLabs[0] is not None and storeLanes is True:
+                    rv[bcLen][1].add(int(cols[colLabs[0]]))
+
+                rv[bcLen][0].append(formatLine(cols, colLabs, indexCols, storeLanes, rcI5))
+
+            # Set columns for barcodes, etc.
+            if lastLine is None:
+                lastLine = True
+                if "index" in cols:
+                    indexCols[0] = cols.index("index")
+                    if "index2" in cols:
+                        indexCols[1] = cols.index("index2")
+
+                if "Lane" in cols:
+                    colLabs[0] = cols.index("Lane")
+
+                if "Sample_ID" in cols:
+                    colLabs[1] = cols.index("Sample_ID")
+
+                if "Sample_Name" in cols:
+                    colLabs[2] = cols.index("Sample_Name")
+
+                if "Sample_Project" in cols:
+                    colLabs[3] = cols.index("Sample_Project")
+                continue
+
+    return reformatSS(rv)
+
+
 def getSampleSheets(d):
     """
     Provide a list of output directories and sample sheets
@@ -35,59 +199,23 @@ def getSampleSheets(d):
 
     laneOut = []
     bcLens = []
+    ssUse = []
     for sheet in ss:
-        # get the lanes
-        lanes = []
-        f = open(sheet)
-        inData = False
-        lastLine = None
-        colNum = None
-        indexCols = [None, None]
-        bcLensAppended = False
-        for line in f:
-            bcLen = '0,0'
-            if inData is False:
-                if line.startswith("[Data]"):
-                    inData = True
-                    continue
-            else:
-                cols = line.strip().split(",")
-                if lastLine is True:
-                    if indexCols[0] is not None:
-                        bcLen = "{}".format(len(cols[indexCols[0]]))
-                    if indexCols[1] is not None:
-                        bcLen = "{},{}".format(bcLen, len(cols[indexCols[1]]))
-                    else:
-                        bcLen += ",0"
-                    bcLens.append(bcLen)
-                    bcLensAppended = True
-                    lastLine = False
+        ss_, laneOut_, bcLens_ = parseSampleSheet(sheet)
+        nSS = 0
+        if ss_ is not None and len(ss_) > 0:
+            ssUse.extend(ss_)
+            nSS = len(ss_)
+        if nSS > 0 and laneOut_ is not None and len(laneOut_) > 0:
+            laneOut.extend(laneOut_)
+        elif nSS > 0:
+            laneOut.extend([None] * nSS)
+        if nSS > 0 and bcLens_ is not None and len(bcLens_) > 0:
+            bcLens.extend(bcLens_)
+        elif nSS > 0:
+            bcLens.extend([None] * nSS)
 
-                # Handle barcodes (once)
-                if "index" in cols and lastLine is None:
-                    indexCols[0] = cols.index("index")
-                    if "index2" in cols:
-                        indexCols[1] = cols.index("index2")
-                        lastLine = True
-                    lastLine = True
-
-                if "Lane" in cols:
-                    colNum = cols.index("Lane")
-                    lastLine = True
-                    continue
-
-                if colNum is not None:
-                    if cols[colNum] != '' and int(cols[colNum]) not in lanes:
-                        lanes.append(int(cols[colNum]))
-        if len(lanes) > 0:
-            lanes = sorted(lanes)
-            laneOut.append("_".join(["{}".format(x) for x in lanes]))
-        if bcLensAppended == False:
-            # This only happens with runs having no Lane or index in the sample sheet
-            bcLens.append(None)
-    if len(ss) == 1:
-        laneOut = [None]
-    return ss, laneOut, bcLens
+    return ssUse, laneOut, bcLens
 
 
 '''
@@ -113,15 +241,18 @@ def newFlowCell(config) :
         config.set('Options','runID',d.split("/")[-2])
 
         # Before 1703 only a single sample sheet was supported
-        if config.get("Options","runID")[:4] < "1703":
+        # Before 1706, parkour wasn't being used, so barcode revComp might be wrong
+        if config.get("Options","runID")[:4] < "1706":
             continue
 
         sampleSheet, lanes, bcLens = getSampleSheets(os.path.dirname(d))
 
         for ss, lane, bcLen in zip(sampleSheet, lanes, bcLens):
             config.set('Options','runID',d.split("/")[-2])
-            if lane is not None:
+            lanesUse = ""
+            if lane is not None and lane != "":
                 config.set("Options","lanes",lane)
+                lanesUse = "_lanes{}".format(lane)
             else:
                 config.set("Options","lanes","")
             if ss is None:
@@ -133,6 +264,14 @@ def newFlowCell(config) :
     
             if flowCellProcessed(config) is False:
                 syslog.syslog("Found a new flow cell: %s\n" % config.get("Options","runID"))
+                odir = "{}/{}{}".format(config.get("Paths", "outputDir"), config.get("Options", "runID"), lanesUse)
+                if not os.path.exists(odir):
+                    os.makedirs(odir)
+                if ss is not None:
+                    o = open("{}/SampleSheet.csv".format(odir), "w")
+                    o.write(ss)
+                    o.close()
+                    ss = "{}/SampleSheet.csv".format(odir)
                 config.set("Options","sampleSheet",ss)
                 return config
             else :
